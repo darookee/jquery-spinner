1e810f344ed34f12ddffd55df9120b0fa91feaf1
darookee <info@darookee.net>
2012-06-05 10:08:25 +0200

    added step functionality

--- jquery.spinner.js
+++ jquery.spinner.js
@@ -4,7 +4,7 @@
 //
 // <div class="spinner">
 //  <button class="increment">+</button>
-//    <input type="number" val="0" min="0" max="0" />
+//    <input type="number" val="0" min="0" max="0" step="1" />
 //  <button class="decrement">-</button>
 // </div>
 //
@@ -18,27 +18,36 @@
           $readout = $this.find('.readout'),
 		      $inc = $this.find('.increment'),
 		      $dec = $this.find('.decrement'),
-		      value = parseInt($readout.val()),
-		      max   = parseInt($readout.attr('max')),
-		      min   = parseInt($readout.attr('min'));
+		      value = parseInt($readout.val(), 10),
+              step = parseInt($readout.attr('step'), 10),
+		      max   = parseInt($readout.attr('max'), 10),
+		      min   = parseInt($readout.attr('min'), 10);
 
+      if( step === 0 || isNaN(step) ) {
+          step = 1;
+      }
           
       // attach buttons to input 
       $inc.live('click', function(){
         if (value < max) {
-          value++;
+          value += step;
+          if( value > max ) {
+            value = max;
+          }
           $readout.val(value);
         }
       });
       
       $dec.live('click', function(){
         if (value > min) {
-          value--;
+          value -= step;
+          if( value < min ) {
+            value = min;
+          }
           $readout.val(value);
         }
       });
 
     });
   };
-})( jQuery );
-
+}(jQuery));
